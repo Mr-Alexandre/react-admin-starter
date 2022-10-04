7e21b6712a04d02e9c8f742c658037502645423e
Alexandr <mr.l.v.alexandre@gmail.com>
2022-10-04 17:01:22 +0600

    add web vitals

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React, { FC, useEffect } from 'react';
 import App from 'src';
 import { createRoot } from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
@@ -17,3 +18,8 @@ const AppWithCallbackAfterRender: FC = () => {
 };
 
 root.render(<AppWithCallbackAfterRender />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
